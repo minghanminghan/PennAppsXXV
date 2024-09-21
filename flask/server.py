ef4465c0098c3f83a4019b7d9624da4d9578b045
minghanminghan <andrewjiang6789@gmail.com>
2024-09-21 14:49:19 -0400

    finished categorization, now working on integrating with flask

--- flask/server.py
+++ flask/server.py
@@ -1,5 +1,7 @@
 # import flast module
 from flask import Flask
+import os
+import categorize as cat
 
 # instance of flask application
 app = Flask(__name__)
@@ -7,7 +9,8 @@ app = Flask(__name__)
 # home route that returns below text when root url is accessed
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    df = cat.csv_to_df('../data/2024.csv')
+    return f"<p>cwd: {os.getcwd()}</p><br><p>df: {df.head()}</p>"
 
 if __name__ == '__main__':  
-   app.run()  
+   app.run()
