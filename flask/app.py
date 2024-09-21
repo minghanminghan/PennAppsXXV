0ce7884ded707e92b63447e1f8468fca38a32f52
minghanminghan <andrewjiang6789@gmail.com>
2024-09-21 18:52:54 -0400

    started working on data visualization

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from models import db
 from views import user_views
+import categorize as cat
+
 
 # Initialize Flask app
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Load configuration
 app.config.from_object('config.Config')
@@ -17,6 +22,18 @@ app.register_blueprint(user_views.bp)
 # Create the database tables within the app context
 with app.app_context():
     db.create_all()
+
+# Routes
+@app.route("/")
+@cross_origin()
+def render():
+    return "app is running!"
+
+@app.route("/data")
+@cross_origin()
+def get_data():
+    df = cat.csv_to_df('../data/2024.csv') # for now, implement data upload + conversion in the future
+    return df.to_json()
 
 if __name__ == '__main__':
     # Run the Flask application
