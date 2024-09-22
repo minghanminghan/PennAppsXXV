89bcb36bed31974369ac5fa0f651ae48fa993b18
Ali Semih Ural <60106172+alisemihural@users.noreply.github.com>
2024-09-22 08:36:46 -0400

    Update about me

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -10,42 +10,6 @@ import Head from 'next/head'; // Correct import for head
 import { withAuthInfo, useLogoutFunction, useRedirectFunctions} from "@propelauth/react";
 
 
-
-
-// // actual render
-// const Home = () => {
-//   const CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
-//   const [line, setLine] = useState(<CanvasJSChart options={{
-//     title: {text: "Line Chart"},
-//     data: [{
-//       type: "column",
-//       dataPoints: [
-//         // { label: "label_1", y: float_1 }, { label: "label_2", y: float_2 }, etc.
-//       ]
-//     }]
-//   }}/>);
-//   const [chart, nextChart] = useState([line]);
-
-//   // declaring states
-//   const response = axios('http://127.0.0.1:5000/data')
-//     .then((res) => { // 
-//       const data = res.data;
-      
-//       return res; // setting global response const
-//     })
-
-
-//   return (
-//     <div>
-//       <button/>
-//       {chart[0]}
-//     </div>
-//   );
-// }
-
-// export default Home;
-
 const SpendWise = ({ isLoggedIn, user }) => {
 const { redirectToLoginPage, redirectToAccountPage, redirectToSignupPage } = useRedirectFunctions();
 const logoutFn = useLogoutFunction();
